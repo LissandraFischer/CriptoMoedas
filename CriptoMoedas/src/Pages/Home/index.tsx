e5e594041993efc33f409773885051d16420678b
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-10-18 16:05:27 -0300

    atualizações 17-10

--- CriptoMoedas/src/Pages/Home/index.tsx
+++ CriptoMoedas/src/Pages/Home/index.tsx
@@ -1,7 +1,33 @@
+import {useEffect, useState} from 'react'
 import styles from "./home.module.css"
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
+import { Link } from "react-router-dom";
+
+
+//https://sujeitoprogramador.com/api-cripto/?key=cc333fdc79bbecb8
+
+
 
 export function Home (){
+
+    const [coins, setCoins] = useState()
+
+    useEffect(()=>{
+        async function getData() {
+            fetch('https://sujeitoprogramador.com/api-cripto/?key=cc333fdc79bbecb8&pref=BRL')
+            .then(response => response.json())
+            .then((data)=>{
+                let coinsData = data.coins.slice(0,15);
+                console.log(coinsData);
+            })
+        }
+
+        getData();
+    },[])
+
+
+
+
     return(
         <main className={styles.container}>
             <form className={styles.form}>
@@ -13,6 +39,42 @@ export function Home (){
                 </button>
 
             </form>
+
+            <table>
+
+                <thead>
+                    <tr>
+
+                        <th scope="col">Moedas</th>
+                        <th scope="col">Valor Mercado</th>
+                        <th scope="col">Preço</th>
+                        <th scope="col">Volume</th>
+
+                    </tr>
+
+                </thead>
+
+                <tbody id="tbody">
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel} data-label="Moeda">
+                            <Link  className={styles.link} to="/detail/btc">
+                                <span>Bitcoin</span> | BTC
+                            </Link>
+                        </td>
+                        <td className={styles.tdLabel}>
+                            R$19293
+                        </td>
+                        <td className={styles.tdLabel}>
+                            R$40.962
+                        </td>
+                        <td className={styles.tdLoss}>
+                            <span> -5.3</span>
+                        </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
 
 
         </main>
