d70debca3209a3d7d1f3943e05e8da258820c5e2
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-10-31 10:18:15 +0000

    atualização final do dia 24/10

--- CriptoMoedas/src/Pages/Detail/index.tsx
+++ CriptoMoedas/src/Pages/Detail/index.tsx
@@ -1,8 +1,55 @@
 import style from './detail.module.css'
 import {useParams} from 'react-router-dom'
+import {useEffect, useState} from 'react'
+
+interface CoinProps{
+    symbol: string;
+    name: string;
+    price: string;
+    market_cap: string;
+    low_24h: string;
+    high_24h:string;
+    total_volume_24h: string;
+    delta_24h: string;
+    volume_24h: string;
+    formatedPrice: string;
+    formatedMarket: string;
+    formatedLowPrice:string;
+    formatedHighPrice: string;
+}
+    
+
 
 export function Detail (){
     const {cripto} = useParams();
+
+    useEffect(() =>{
+        function getData(){
+            fetch(` https://sujeitoprogramador.com/api-cripto/coin/?key=cc333fdc79bbecb8&symbol=${cripto}`)
+            .then(response => response.json())
+            .then((data:CoinProps) => {
+
+                let price = Intl.NumberFormat ("pt-BR",{
+
+                    style: "currency",
+                    currency:"BRL"
+                })
+
+                const resultData = {
+                    ...data,
+                    formatedPrice: price.format(Number(data.price)),
+                    formatedMarket: price.format(Number(data.market_cap)),
+                    formatedLowPrice: price.format(Number(data.low_24h)),
+                    formatedHighPrice: price.format(Number(data.high_24h_24h)),
+
+                }
+                console.log(resultData);
+
+            })
+        }
+        getData();
+    },[cripto])
+
     return(
         <div>
             <h1>Pagina de detalhes {cripto}</h1>
